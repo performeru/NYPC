3de2abf1587d7321251187c416ccc4b68458183c
performeru <130340605+performeru@users.noreply.github.com>
2024-06-07 21:36:14 +0900

    Update 마비노기 듀얼: 올바른 덱인가요?.cpp

--- NYPC 2016/ 2. 마비노기 듀얼: 올바른 덱인가요?/마비노기 듀얼: 올바른 덱인가요?.cpp
+++ NYPC 2016/ 2. 마비노기 듀얼: 올바른 덱인가요?/마비노기 듀얼: 올바른 덱인가요?.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
-  
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N;
+  cin >> N;
+
+  unordered_map<string, int> resource_count;
+  string resource;
+    
+  for (int i = 0; i < N; ++i) 
+  {
+    cin >> resource;
+    resource_count[resource]++;
+  }
+
+  if (resource_count.size() <= 3) 
+  {
+    cout << "valid" << "\n";
+  }
+  else
+  {
+    cout << "invalid" << "\n";
+  }
 }
