108e3a32590da520d54f3dca2b542a6447c87eb1
performeru <130340605+performeru@users.noreply.github.com>
2024-07-24 11:04:39 +0900

    Update 돌리고 밀고.cpp

--- NYPC 2016/본선 1. 돌리고 밀고/돌리고 밀고.cpp
+++ NYPC 2016/본선 1. 돌리고 밀고/돌리고 밀고.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main()
+vector<string> rotate90(const vector<string>& board, int N) 
 {
-  
+    vector<string> newBoard(N, string(N, '.'));
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < N; ++j) 
+        {
+            newBoard[j][N - 1 - i] = board[i][j];
+        }
+    }
+    return newBoard;
+}
+
+vector<string> slideDown(const vector<string>& board, int N) 
+{
+    vector<string> newBoard(N, string(N, '.'));
+    for (int j = 0; j < N; ++j) 
+    {
+        int emptyRow = N - 1;
+        for (int i = N - 1; i >= 0; --i) 
+        {
+            if (board[i][j] != '.') 
+            {
+                newBoard[emptyRow][j] = board[i][j];
+                --emptyRow;
+            }
+        }
+    }
+    return newBoard;
+}
+
+vector<string> spinAndSlide(const vector<string>& board, int N, int M) 
+{
+    vector<string> currentBoard = board;
+    for (int i = 0; i < M; ++i) 
+    {
+        currentBoard = rotate90(currentBoard, N);
+        currentBoard = slideDown(currentBoard, N);
+    }
+    return currentBoard;
+}
+
+int main() 
+{
+    int N, M;
+    cin >> N >> M;
+    vector<string> board(N);
+    for (int i = 0; i < N; ++i) 
+    {
+        cin >> board[i];
+    }
+
+    vector<string> result = spinAndSlide(board, N, M);
+
+    for (const string& row : result) 
+    {
+        cout << row << endl;
+    }
+
 }
